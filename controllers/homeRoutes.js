46afe224d2742f192f87582bbd24ff59bc3bf65a
Bryan lara <72678809+BRYLARA@users.noreply.github.com>
2021-04-05 21:26:03 -0600

    more updates with herou and package json

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,29 +3,29 @@ const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  res.status(220).json({message:"hello"})
-  // try {
-  //   // Get all projects and JOIN with user data
-  //   const projectData = await Project.findAll({
-  //     include: [
-  //       {
-  //         model: User,
-  //         attributes: ['name'],
-  //       },
-  //     ],
-  //   });
 
-  //   // Serialize data so the template can read it
-  //   const projects = projectData.map((project) => project.get({ plain: true }));
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-  //   // Pass serialized data and session flag into template
-  //   res.render('homepage', { 
-  //     projects, 
-  //     logged_in: req.session.logged_in 
-  //   });
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/project/:id', async (req, res) => {
