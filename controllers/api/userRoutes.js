d048616eafdf6f14d92d525df40797e5963c4d8a
b00kbaker <caimmurphy@gmail.com>
2021-04-13 17:56:47 -0600

    Cleaned up file structure

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const { Faves } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
@@ -17,33 +16,6 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.post( '/:id/faves')
-//fetch url /api/users/42/faves req.params.id
-
-// router.post('/faves', async (req, res) => {
-//   try {
-//     const faveData
-//   }
-// })
-//fetch url /api/users/faves  req.body.user_id
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const favesData = await Faves.findAll(req.body.user_id);
-//     res.status(200).json(favesData);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-router.post('/', async (req, res) => {
-  try {
-    const favesData = await Faves.create(req.body.user_id);
-    res.status(200).json(favesData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 
 router.post('/login', async (req, res) => {
   try {
