e5bff7099fc3a4455413d283223f42b102cba96c
David Bushard <davidbushard@gmail.com>
2021-04-14 18:49:21 -0700

    New CSS and its also grabbing the data from the DB just cant display it yet.

--- controllers/api/faveRoutes.js
+++ controllers/api/faveRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Faves } = require("../../models");
+const { Faves , User } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post('/', withAuth, async (req, res) => {
@@ -18,38 +18,38 @@ router.post('/', withAuth, async (req, res) => {
 
 // Lines 21-45 kept landing on 'catch 500' never loaded data
 
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const parkData = await Faves.findAll ({
-//       include: [
-//         {
-//           model: Faves,
-//           attributes: [
-//             fullName,
-//             description,
-//           ],
-//         },
-//       ],
-//     });
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const parkData = await Faves.findOne ({
+      include: [
+        {
+          model: User,
+          // attributes: [
+          //   "fullName",
+          //   "description",
+          // ],
+        },
+      ],
+    });
+console.log(parkData);
+    const favorite = parkData.get({ plain: true });
+    res.render('faves', {
+      parkData,
+      logged_in: req.session.logged_in,
+    });
 
-//     const favorite = parkData.get({ plain: true });
-//     res.render('faves', {
-//       faves,
-//      logged_in: req.session.logged_in,
-//     });
-
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
+});
 
 
 
 // '/api/faves' Displaying correct handlebars
 
-router.get('/', async (req,res) => {
-  res.render('faves')
-});
+// router.get('/', async (req,res) => {
+//   res.render('faves')
+// });
 
 module.exports = router;
